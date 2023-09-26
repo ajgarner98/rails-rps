8c1d4bdec953bc108ea396d3dfa10c5cb5f9eb8e
Andre Garner <111984130+ajgarner98@users.noreply.github.com>
2023-09-26 15:51:59 +0000

    Finished the game logic

--- app/controllers/zebra_controller.rb
+++ app/controllers/zebra_controller.rb
@@ -1,5 +1,56 @@
 class ZebraController < ApplicationController
+  @game_results = ""
+  @enemy_play = ""
   def giraffe
-    render({plain: => "Howdy"})
+    @enemy_random = rand(1..3)
+    if @enemy_random == 1
+      @enemy_play = "They played paper!"
+      @game_results ="We lost!"
+    elsif @enemy_random == 2
+      @enemy_play = "They played scissors!"
+      @game_results = "We won!"
+      
+    else
+      @enemy_play = "They played rock!"
+      @game_results = "We tie!"
+    end
+
+    render({:template => "game_templates/play_rock"})
+  end
+
+  def tiger
+    @enemy_random = rand(1..3)
+    if @enemy_random == 1
+      @enemy_play = "They played paper!"
+      @game_results ="We tied!"
+    elsif @enemy_random == 2
+      @enemy_play = "They played scissors!"
+      @game_results = "We lost!"
+      
+    else
+      @enemy_play = "They played rock!"
+      @game_results = "We won!"
+    end
+    render({:template => "game_templates/play_paper"})
+  end
+
+  def elephant
+    @enemy_random = rand(1..3)
+    if @enemy_random == 1
+      @enemy_play = "They played paper!"
+      @game_results ="We won!"
+    elsif @enemy_random == 2
+      @enemy_play = "They played scissors!"
+      @game_results = "We tied!"
+      
+    else
+      @enemy_play = "They played rock!"
+      @game_results = "We lost!"
+    end
+    render({:template => "game_templates/play_scissors"})
+  end
+
+  def shark
+    render({:template => "game_templates/home"})
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
-  get("rock", {:controller => "zebra", :action =>  "giraffe"})
+  get("/rock", { :controller => "zebra", :action =>  "giraffe"})
+  get("/paper", { :controller => "zebra", :action => "tiger"})
+  get("/scissors", {:controller => "zebra", :action => "elephant"})
+  get("/", {:controller => "zebra", :action => "shark"})
 end
